226c5b81ce26cd71583fee4e2b7d2ddb00abbe15
ciamac <hgzguzg>
2020-06-06 21:14:16 +0200

    Verified by Cia v1.0.3

--- redux-basics.js
+++ redux-basics.js
@@ -3,7 +3,8 @@ const redux = require('redux');
 // createStore is a Function that allows us to make a new store!
 const createStore = redux.createStore;
 const initialState = {
-      counter: 0
+      counter: 0,
+
 }
 
 
@@ -12,7 +13,21 @@ const initialState = {
 // connected to the stores and its only thing tham maybe update the state at the end!
 // therefore i nedd to make my reducer first!
 const rootReducer = (state = initialState, action) => {
-return state; 
+      switch(action.payload){
+      case "INC-COUNTER": 
+            return {
+                  ...state,
+                  counter: state.counter + 1
+            };
+      
+      case "ADD-COUNTER":
+            return {
+                  ...state,
+                  counter: state.counter + action.value
+            };
+      
+       default: return state; 
+}
 };
 
 
@@ -23,8 +38,13 @@ console.log(store.getState());
 
 
 
-// Dispatching Action 
 
+// Dispatching Action 
+// type is just a unique identify of your choice
+// property name is totally up to you! in this case value:10 i mean
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',  payload:{ value: 10}});
+console.log(store.getState());
 
 
 // Subscription
