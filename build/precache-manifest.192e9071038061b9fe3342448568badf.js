efce8195d8007def67a72cb2d4d5b195625d35e0
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 10:32:38 +0100

    Verified by Marti4n

---
+++ build/precache-manifest.192e9071038061b9fe3342448568badf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3934f70ede15e6cf5dd35869a1c969",
+    "url": "/react-redux-increment-decrement/index.html"
+  },
+  {
+    "revision": "d2a264f0821023a01dbc",
+    "url": "/react-redux-increment-decrement/static/css/main.6e44ffb7.chunk.css"
+  },
+  {
+    "revision": "99d5e43ea15b1996f4f4",
+    "url": "/react-redux-increment-decrement/static/js/2.4b080ab9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-increment-decrement/static/js/2.4b080ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a264f0821023a01dbc",
+    "url": "/react-redux-increment-decrement/static/js/main.c42b653a.chunk.js"
+  },
+  {
+    "revision": "dec565cf3cbd95f50c69",
+    "url": "/react-redux-increment-decrement/static/js/runtime-main.228e28f4.js"
+  }
+]);
